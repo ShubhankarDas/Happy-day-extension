e20a3e2dc12fa26f435cd5c64c4a081c7a148041
Shubhankar Das <shubh065@gmail.com>
2020-11-22 01:06:10 +0530

    add new images

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,17 @@
-const functions = require('firebase-functions');
-const firebaseAdmin = require('firebase-admin');
-const express = require('express');
-const engines = require('consolidate');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const functions = require("firebase-functions");
+const firebaseAdmin = require("firebase-admin");
+const express = require("express");
+const engines = require("consolidate");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 firebaseAdmin.initializeApp();
 const db = firebaseAdmin.firestore();
 
 let quotesList = [];
 
-async function getQuptes() {
+async function getQuotes() {
   const data = [];
-  const snapshot = await db.collection('quotes').get();
+  const snapshot = await db.collection("quotes").get();
   snapshot.forEach((doc) => {
     data.push(doc.data());
   });
@@ -20,73 +20,84 @@ async function getQuptes() {
 }
 
 async function getRandomQuote() {
-  if (quotesList.length === 0) quotesList = await getQuptes();
+  if (quotesList.length === 0) quotesList = await getQuotes();
   const random = Math.floor(Math.random() * quotesList.length);
   return quotesList[random];
 }
 
-async function addQuoteMiddleware(req, res, next) {
-  let { quote, image_name } = req.body;
-  if (!quote || quote.length < 1) {
-    return res.status(400).send({
-      status: 400,
-      message: 'Quote missing',
-    });
-  }
-  let images = await getImageList();
-  if (!image_name || image_name.length < 1 || !images.includes(image_name)) {
-    return res.status(400).send({
-      status: 400,
-      message: 'Image_name missing',
-    });
-  }
-  return next();
-}
-
-async function addQuote({ quote, image_name }) {
-  const res = await db.collection('quotes').add({
+async function addQuote({ quote, image_name, image_link }) {
+  const res = await db.collection("quotes").add({
     quote,
     image_name,
+    image_link,
   });
-  return 'Quote added';
+  return "Quote added";
 }
 
 async function getAllQuotes() {
-  quotesList = await getQuptes();
+  quotesList = await getQuotes();
   return quotesList;
 }
 
 async function getImageList() {
   let quotes = await getAllQuotes();
-  let images = quotes.map((q) => q.image_name);
-  return [...new Set(images)];
+  let images = quotes
+    .filter((q) => !q.image_link)
+    .map((q) => `./images/${q.image_name}`);
+  let links = quotes.filter((q) => q.image_link).map((q) => q.image_link);
+  return [...new Set(images), ...new Set(links)];
+}
+
+async function addQuoteMiddleware(req, res, next) {
+  let { quote, image_name, image_link } = req.body;
+  if (!quote || quote.length < 1) {
+    return res.status(400).send({
+      status: 400,
+      message: "Quote missing",
+    });
+  }
+  if (!image_name || image_name.length < 1) {
+    return res.status(400).send({
+      status: 400,
+      message: "Image_name missing",
+    });
+  }
+  let images = await getImageList();
+  if ((!image_link || image_link.length < 2) && !images.includes(image_name)) {
+    return res.status(400).send({
+      status: 400,
+      message: "Image is not present in the list",
+    });
+  }
+  return next();
 }
 
 const app = express();
-app.engine('hbs', engines.handlebars);
-app.set('views', './views');
-app.set('view engine', 'hbs');
+app.engine("hbs", engines.handlebars);
+app.set("views", "./views");
+app.set("view engine", "hbs");
 
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
 app.use(bodyParser.json());
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   quote = await getRandomQuote();
   if (quote) {
-    return res.render('home', {
+    return res.render("home", {
       image_name: quote.image_name,
       message: quote.message,
+      image_link: quote.image_link,
     });
   } else {
-    return res.render('home', {
-      image: '',
-      message: 'Something went wrong',
+    return res.render("home", {
+      image: "",
+      message: "Something went wrong",
     });
   }
 });
 
-app.get('/quote', async (req, res) => {
+app.get("/quote", async (req, res) => {
   quote = await getRandomQuote();
   if (quote) {
     return res.status(200).send({
@@ -100,14 +111,21 @@ app.get('/quote', async (req, res) => {
   }
 });
 
-app.get('/admin', async (req, res) => {
+app.get("/admin", async (req, res) => {
   let images = await getImageList();
-  return res.render('admin', {
+  return res.render("admin", {
     images: images,
   });
 });
 
-app.get('/quotes', async (req, res) => {
+app.get("/images", async (req, res) => {
+  let images = await getImageList();
+  return res.status(200).send({
+    images: images,
+  });
+});
+
+app.get("/quotes", async (req, res) => {
   quotes = await getAllQuotes();
   if (quotes) {
     return res.status(200).send({
@@ -121,7 +139,7 @@ app.get('/quotes', async (req, res) => {
   }
 });
 
-app.post('/quote', addQuoteMiddleware, async (req, res) => {
+app.post("/quote", addQuoteMiddleware, async (req, res) => {
   result = await addQuote(req.body);
   return res.status(200).send({
     status: 200,
@@ -130,7 +148,7 @@ app.post('/quote', addQuoteMiddleware, async (req, res) => {
 });
 
 app.use((req, res, next) => {
-  res.redirect('/');
+  res.redirect("/");
 });
 
 exports.app = functions.https.onRequest(app);
--- public/admin.js
+++ public/admin.js
@@ -1,47 +1,112 @@
-$('.card').click(function () {
-  $('.card-container').addClass('hide');
-  $('.message-container').removeClass('hide');
-  $('#save-cta').removeClass('hide');
-  $('.hero-image').attr('src', $(this).find('.card-image').attr('src'));
+const Message = {
+  hasNewImage: false,
+};
+
+$(".select").click(function () {
+  Message.hasNewImage = false;
+  $(".card-container").addClass("hide");
+  $(".message-container").removeClass("hide");
+  $("#save-cta").removeClass("hide");
+  $(".hero-image").attr("src", $(this).find(".card-image").attr("src"));
+  $(".hero-image").removeClass("hide");
+  $(".add-image").addClass("hide");
 });
 
-$('#back-btn').click(function () {
-  $('.card-container').removeClass('hide');
-  $('.message-container').addClass('hide');
-  $('#save-cta').addClass('hide');
+$(".add").click(function () {
+  Message.hasNewImage = true;
+  $(".card-container").addClass("hide");
+  $(".message-container").removeClass("hide");
+  $(".hero-image").addClass("hide");
+  $(".add-image").removeClass("hide");
 });
 
-$('#save-cta:not(.disabled)').click(function () {
-  let saveBtn = $('#save-cta');
-  saveBtn.addClass('disabled');
-  saveBtn.text('SAVING..');
-  let image_src_split = $('.hero-image').attr('src').split('/');
-  let quote = $('#message').val();
-  let image_name = image_src_split[image_src_split.length - 1];
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    let reader = new FileReader();
 
-  if (!isValid(quote) || !isValid(image_name)) {
-    return;
+    reader.onload = function (e) {
+      $(".hero-image").attr("src", e.target.result);
+      $(".hero-image").removeClass("hide");
+      $(".add-image").addClass("hide");
+    };
+    reader.readAsDataURL(input.files[0]);
+    $("#save-cta").removeClass("hide");
   }
+}
 
-  let data = {
-    quote,
-    image_name,
-  };
+$("#back-btn").click(function () {
+  $(".card-container").removeClass("hide");
+  $(".message-container").addClass("hide");
+  $("#save-cta").addClass("hide");
+});
 
-  $.ajax({
-    url: './quote',
-    type: 'POST',
-    data: JSON.stringify(data),
-    contentType: 'application/json',
-    dataType: 'json',
-    success: function () {
-      saveBtn.removeClass('disabled');
-      saveBtn.text('SAVE');
-      $('#back-btn').trigger('click');
-    },
-  });
+$("#save-cta:not(.disabled)").click(function () {
+  let saveBtn = $("#save-cta");
+  saveBtn.addClass("disabled");
+  saveBtn.text("SAVING..");
+
+  getData()
+    .then((data) => {
+      console.log(data);
+      $.ajax({
+        url: "./quote",
+        type: "POST",
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        dataType: "json",
+        success: function () {
+          saveBtn.removeClass("disabled");
+          saveBtn.text("SAVE");
+          $("#back-btn").trigger("click");
+        },
+      });
+    })
+    .catch((err) => {
+      console.log("could not upload");
+      console.log(err.message);
+    });
 });
 
 function isValid(field) {
   return field.length > 4;
+}
+
+function getData() {
+  return new Promise((resolve, reject) => {
+    let imageSrcSplit = $(".hero-image").attr("src").split("/");
+    let quote = $("#message").val();
+    let imageName = imageSrcSplit[imageSrcSplit.length - 1];
+
+    if (!isValid(quote)) {
+      return reject(new Error("Quote not fond"));
+    }
+
+    if (Message.hasNewImage) {
+      let newImage = $(".add-image").prop("files")[0];
+      if (!newImage) {
+        return reject(new Error("New image not fond"));
+      }
+      imageName = `${new Date().getTime()}-${newImage.name}`;
+      let storageRef = storage.ref("images/");
+      return storageRef
+        .child(imageName)
+        .put(newImage)
+        .then((snapshot) => {
+          snapshot.ref.getDownloadURL().then((downloadURL) => {
+            return resolve({
+              quote,
+              image_name: imageName,
+              image_link: downloadURL,
+            });
+          });
+        });
+    }
+    if (!isValid(imageName)) {
+      return reject(new Error("imageName not fond"));
+    }
+    return resolve({
+      quote,
+      image_name: imageName,
+    });
+  });
 }
